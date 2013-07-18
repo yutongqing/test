0c02d37435eb7206b0f99bd46c50133da791f367
Yu <yu@Yus-Mac-mini.local>
2013-07-18 15:50:09 +0800

    Initial Commit

---
+++ AngelPanda/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AngelPanda
+//
+//  Created by Yu on 7/18/13.
+//  Copyright Yu 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
